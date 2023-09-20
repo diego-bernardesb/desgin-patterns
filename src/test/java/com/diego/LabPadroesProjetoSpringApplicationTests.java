62432abe4bdb3fe1462ae4f26874698228606a91
Diego Bernardes Babo <diegobbabo@gmail.com>
2023-09-20 11:23:48 -0300

    frist commit

---
+++ src/test/java/com/diego/LabPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
